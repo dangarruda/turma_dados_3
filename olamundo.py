5679ec8cc443105df1b10352754b702ada391387
daniel <dangarruda@gmail.com>
2025-10-13 21:01:42 -0300

    entrada da idade do usuario

--- olamundo.py
+++ olamundo.py
@@ -1,1 +1,12 @@
 print('ola, mundo!')
+
+nome = input('digite seu nome')
+print('ola, '+ nome +'!')
+
+idade = int(input('digite sua idade'))
+if (idade >= 18):
+    print('você é maior de idade.')
+else:
+    print('você é menor de idade')
+
+
